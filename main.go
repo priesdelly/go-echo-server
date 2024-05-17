c87e3f5e13b76430279b3f732280f09b55aa1ba4
priesdelly <priesdelly@gmail.com>
2024-05-17 09:44:43 +0700

    feat: release v1

--- main.go
+++ main.go
@@ -1,22 +1,70 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
 
-type header struct {
-}
-
-type body struct {
-}
-
-type res struct {
-	method string
-	path   string
-	header header
-	body   body
+type HTTPRequestDetails struct {
+	Method string              `json:"method"`
+	URL    string              `json:"url"`
+	Header map[string][]string `json:"header"`
+	Body   string              `json:"body"`
 }
 
 func main() {
 	log.Println("Start App")
+	http.HandleFunc("/", rootRouteHandler)
 
-	log.Println(("End App"))
+	log.Println("Server listening on port 8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("End App")
+}
+
+func rootRouteHandler(w http.ResponseWriter, r *http.Request) {
+	// Extract request details
+	details := HTTPRequestDetails{
+		Method: r.Method,
+		URL:    r.URL.String(),
+		Header: r.Header,
+	}
+
+	// Read request body
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println("Error reading request body:", err)
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+	defer func() {
+		if err := r.Body.Close(); err != nil {
+			log.Println("Error closing request body:", err)
+		}
+	}() // Close the body even in case of other errors
+
+	details.Body = string(body)
+
+	// Marshal request details to JSON
+	jsonData, err := json.Marshal(details)
+	if err != nil {
+		log.Println("Error marshalling request details to JSON:", err)
+		http.Error(w, "Error marshalling request body", http.StatusInternalServerError)
+		return
+	}
+
+	// Set content type and write JSON response
+	w.Header().Set("Content-Type", "application/json")
+	if _, err := w.Write(jsonData); err != nil {
+		log.Println("Error writing JSON response:", err)
+		// Consider returning a more specific error code depending on the nature of the write error
+		http.Error(w, "Error writing response", http.StatusInternalServerError)
+		return
+	}
 }
